e7ad1c81d632fe72ef7c4b63587248ebd84522c1
shitizz <110281838+shitizz@users.noreply.github.com>
2024-11-07 20:27:32 +0530

    Delete rids_spotify directory

--- rids_spotify/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# myapp/admin.py
-
-from django.contrib import admin
-from .models import UserRegistration
-
-admin.site.register(UserRegistration)
--- rids_spotify/myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import base,login
-urlpatterns = [
-    path('', base, name='base'),
-    path('login/',login,name="login")  # Use an appropriate URL path
-]
